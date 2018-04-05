50b4e2f22811d833991906b0daad89640ffa60f5
Kevin Foster <kevin.foster.uk@gmail.co>
2018-04-05 12:00:43 +0100

    Support dataPaths and paramPaths options for passing related data

--- src/remote.js
+++ src/remote.js
@@ -1,17 +1,21 @@
 'use strict'
 var axios = require('axios');
 var Validator = require('mzen-schema/lib/schema/validator');
+var ObjectPathAccessor = require('mzen-schema/object-path-accessor');
 
-function doRemote(value, options, name, root) 
+function doRemote(value, options, name, root)
 {
   var url = options && options['url'] ? options['url'] : '';
   var method = options && options['method'] ? options['method'] : 'POST';
   var params = options && options['params'] ? options['params'] : {};
+  var paramPaths = options && options['paramPaths'] ? pathsToObject(options['paramPaths'], root) : {};
   var data = options && options['data'] ? options['data'] : {};
+  var dataPaths = options && options['dataPaths'] ? pathsToObject(options['dataPaths'], root) : {};
   var timeout = options && options['timeout'] ? options['timeout'] : 5000;
   var axiosInstance = options && options['axios'] ? options['axios'] : axios.create({timeout: timeout});
 
-  data['value'] = value;
+  params = Object.assign({}, paramPaths, params);
+  data = Object.assign({}, dataPaths, data, {value});
 
   return axiosInstance({
     url: url,
@@ -21,7 +25,16 @@ function doRemote(value, options, name, root)
   });
 }
 
-Validator.remote = function(value, options, name, root) 
+function pathsToObject(paths, data)
+{
+  var result = {};
+  Object.keys(paths).forEach((fieldName) => {
+    result[fieldName] = ObjectPathAccessor.getPath(paths[fieldName]);
+  });
+  return result;
+}
+
+Validator.remote = function(value, options, name, root)
 {
   var promise = new Promise((resolve, reject) => {
     doRemote.apply(null, arguments).then((response) =>{
